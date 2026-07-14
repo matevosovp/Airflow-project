affe79db136a1d47f63976428e47b57e9d8cbe70
matevosovp <matevosovp@gmail.com>
2026-07-14 13:37:04 +0300

    Centralize Airflow table configuration

--- part1_airflow/plugins/config.py
+++ part1_airflow/plugins/config.py
@@ -1,12 +1,15 @@
 from airflow.datasets import Dataset
 
 PG_CONN_ID = "pg_conn"
+DB_SCHEMA = "public"
 
-RAW_TABLE = "public.real_estate_dataset_raw"
-CLEAN_TABLE = "public.real_estate_dataset_clean"
+FLATS_TABLE = f"{DB_SCHEMA}.flats"
+BUILDINGS_TABLE = f"{DB_SCHEMA}.buildings"
 
-RAW_DATASET_URI = f"postgres://{PG_CONN_ID}/{RAW_TABLE}"
-CLEAN_DATASET_URI = f"postgres://{PG_CONN_ID}/{CLEAN_TABLE}"
+RAW_TABLE = f"{DB_SCHEMA}.real_estate_dataset_raw"
+RAW_STAGING_TABLE = f"{DB_SCHEMA}.real_estate_dataset_raw_stg"
+CLEAN_TABLE = f"{DB_SCHEMA}.real_estate_dataset_clean"
+CLEAN_STAGING_TABLE = f"{DB_SCHEMA}.real_estate_dataset_clean_stg"
 
-RAW_DATASET = Dataset(RAW_DATASET_URI)
-CLEAN_DATASET = Dataset(CLEAN_DATASET_URI)
+RAW_DATASET = Dataset(f"postgres://{PG_CONN_ID}/{RAW_TABLE}")
+CLEAN_DATASET = Dataset(f"postgres://{PG_CONN_ID}/{CLEAN_TABLE}")
